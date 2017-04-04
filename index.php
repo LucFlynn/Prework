d7e1b3bdba99e0fd56a51d85431675ddd1dde97b
Luc Flynn <lflynn7@cnm.edu>
2017-04-04 13:42:14 -0600

    added starter index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>I Completed the prework</title>
+	</head>
+	<body>
+		<p>I Completed the prework!</p>
+		<p> Aweuh Yea buddy!!!!</p>
+		<p> Now I can effectively work through all that prework book stuff.</p>
+	</body>
+</html>
