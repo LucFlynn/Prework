d45624827a6ddc8578601a485d916a035efec244
Luc Flynn <lflynn7@cnm.edu>
2017-04-05 12:13:50 -0600

    Working out of the book

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo <<<_END
 			<title>Form Test</title>
 		</head>
 		<body>
-		<form method="post" action="formtest.php">
+		<form method="post" action="https://bootcamp-coders.cnm.edu/~lflynn7/prework/index.php">
 		What is your name?
 		<input type="text" name="name">
 		<input type="submit">
