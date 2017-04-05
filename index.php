33f221c4c9577f95270852a4becaa4959c18a61a
Luc Flynn <lflynn7@cnm.edu>
2017-04-05 12:12:32 -0600

    added starter index page

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>I Completed the prework</title>
-	</head>
-	<body>
-		<p>I Completed the prework!</p>
-		<p> Aweuh Yea buddy!!!!</p>
-
-	</body>
+<?php // formtest.php
+echo <<<_END
+	<html>
+		<head>
+			<title>Form Test</title>
+		</head>
+		<body>
+		<form method="post" action="formtest.php">
+		What is your name?
+		<input type="text" name="name">
+		<input type="submit">
+</form>
+</body>
 </html>
+_END;
+?>
